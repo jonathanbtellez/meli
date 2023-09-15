9b43e4490977aa3b3dd6bc537a40d2a0748b976d
jonathanbtellez <jonathanbtellez@gmail.com>
2023-09-15 15:21:46 -0500

    Config environment

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+import vSelect from "vue-select";
+
+// Components ---------------------------------------------------
+// import TheBookList from './components/Books/TheBookList.vue'
+
+const app = createApp({
+  components: {
+    // TheBookList
+  },
+});
+
+app.component("v-select", vSelect);
+app.mount("#app");
